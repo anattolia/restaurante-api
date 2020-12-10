e3046fce5c83eda10e5f303a953c002edacb1b7c
Antonia Bustamante <anattolia@gmail.com>
2020-12-10 12:42:36 -0500

    corrección url /

--- main.py
+++ main.py
@@ -23,9 +23,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost", 
     "http://localhost:8080",
-    "http://localhost:8081",
-    "127.0.0.1:8000", 
-    "127.0.0.1:8081", 
+    "http://localhost:8081", 
     "https://restaurante-app-antonia.herokuapp.com"
 
 ]
